a719c96604f23e7ea3544fef487eb7c3fc1ff598
Mohamed Hisham <Mohamed00Hisham@Gmail.com>
2019-01-29 23:38:07 +0200

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+/*
+Problem: 
+Link   : 
+
+solution by: Mohamed Hisham El-Banna
+
+Gmail   : Mohamed00Hisham@Gmail.com
+Github  : www.github.com/Mhmd-Hisham
+LinkedIn: www.linkedin.com/in/Mhmd-Hisham
+*/
+
+
+using namespace std;
+
+typedef signed long long ll;
+typedef unsigned long long ull;
+
+#define FOR(i,to) for(i=0;i<(to);i++)
+#define FORR(i,arr) for(auto& i:arr)
+#define ITER(it,cont) for(__typeof(cont.begin()) it=cont.begin();it!=cont.end();it++)
+#define ALL(v) (v.begin()),(v.end())
+#define ZERO(arr) memset(arr,0,sizeof(arr))
+#define MINUS(arr) memset(arr,0xff,sizeof(arr))
+
+int N = 0, T = 0;
+double N = 0;
+
+int A[];
+double A[];
+
+int t = 0;
+double t = 0;
+
+int A = 0, B = 0, C = 0;
+double A = 0, B = 0, C = 0;
+
+int x = 0, y = 0;
+double x = 0, y = 0;
+
+
+int main () {
+
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> A >> B >> C;
+    cin >> x >> y;
+
+    cin >> N;
+    scanf("%d\n", &N);
+
+    cin >> T;
+    scanf("%d\n", &T);
+    
+    while ( cin >> N && !cin.eof() ){
+        
+    }
+    
+    while ( T-- ){
+        scanf("%d\n", &T);
+    
+    }
+    
+    while ( scanf("%d\n", &N) && N ){
+    
+    }
+    
+    for (int i = 0; i < N; i++){
+        scanf("%d\n", &T);
+        cin >> A[i];
+        cin >> t;
+    }
+
+    printf("%.3f\n", );
+    printf("%d\n", );
+    cout << N << '\n';
+    return 0;
+}
+/*
+Sample input:-
+-----------------
+
+
+Sample output:-
+-----------------
+
+
+Resources:-
+-------------
+
+Explanation:
+---------------
+
+*/
